5432e560294f18079dedbb62b7e72c4a42bbc180
nagamachia <7518545@alumni.tus.ac.jp>
2021-05-09 14:28:12 +0900

    updated abc200 d

--- abc200/d.py
+++ abc200/d.py
@@ -1,15 +1,30 @@
-import itertools as itrt
+from itertools import product
+from collections import Counter
 
 N = int(input())
-A = int(input().split())
+A = list(map(lambda x: int(x)%200, input().split()))
 
 sums = []
-ls = []
 
-for i in range(1,N):
-    for l in itrt.combinations(A, i):
-        ls.append(l)
-        s = sum(l)%200
-        if s in set(sums):
-            sums[sums.index(s)]
-        sums.append()
+bins = list(product([False, True], repeat=min(N,8)))[1:]
+
+for b in bins:
+    s=0
+    for i in range(min(N,8)):
+        if b[i]:
+            s+=A[i]
+    sums.append(s%200)
+
+if max(Counter(sums).values())<2:
+    print("No")
+else:
+    print("Yes")
+    for k,v in Counter(sums).items():
+        if v>=2:
+            pair = [i for i, s in enumerate(sums) if s==k][:2]
+            for p in pair:
+                x = sum(bins[p])
+                B = [i+1 for i, b in enumerate(bins[p]) if b]
+                print(x,*B)
+            exit()
+
