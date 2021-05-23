9de912964e5b76d226920b2d400d00d1513e2dbd
nagamachia <7518545@alumni.tus.ac.jp>
2021-05-23 18:32:22 +0900

    solved abc202 c,d

--- abc202/c.py
+++ abc202/c.py
@@ -1,4 +1,4 @@
-from collections import Counter
+from collections import Counter,defaultdict
 
 N=int(input())
 
@@ -6,26 +6,20 @@ A=list(map(int,input().split()))
 B=list(map(int,input().split()))
 C=list(map(int,input().split()))
 
-cntA = Counter(A)
-cntC = Counter(C)
+cntA = defaultdict(int)
+for k,v in Counter(A).items():
+    cntA[k] = v
 
 ans=0
 
-# cntB={}
-# B = [b for b in B if b in cntA.keys()]
-# for k,v in cntC.items():
-#     cntB[]
+# for k,v in cntA.items():
+#     ansk=0
+#     idx = [i for i,x in enumerate(B) if x==k]
+#     for i in idx:
+#         ansk+=cntC[i+1]
+#     ans+=ansk*v
 
-
-for k,v in cntA.items():
-    ansk=0
-    idx = [i for i,x in enumerate(B) if x==k]
-    for i in idx:
-        ansk+=cntC[i+1]
-    # for i,x in enumerate(B):
-    #     if x==k:
-    #         ansk+=cntC[i+1]
-    ans+=ansk*v
-
+for c in C:
+    ans+=cntA[B[c-1]]
 
 print(ans)
--- abc202/d.py
+++ abc202/d.py
@@ -1,2 +1,44 @@
+# from itertools import accumulate
+from math import factorial
+
 A,B,K = map(int,input().split())
 
+# comb = [0]*(A+1)
+# comb[0] = 1
+# comb[1] = B
+
+# for i in range(2,A+1):
+#     comb[i] = comb[i-1]*(B+i-1)/i
+
+# combSum = list(accumulate(comb))
+# # ans = list("a"*A+"b"*B)
+# combSum.reverse()
+# ans = ""
+
+# for i in range(A+1):
+#     if K <= combSum[i]:
+#         ans[B+i-1] = "b"
+#         res = K-combSum[i-1]
+#     else:
+#         ans+="a"
+
+def comb(n, r):
+    return factorial(n) // (factorial(n - r) * factorial(r))
+
+a = A
+b = B
+ans = ""
+
+while a>0 and b>0:
+    acomb = comb(a+b-1,a-1)
+    if K <= acomb:
+        ans+="a"
+        a-=1
+    else:
+        ans+="b"
+        K-=acomb
+        b-=1
+ans+="a"*a
+ans+="b"*b
+
+print(ans)
