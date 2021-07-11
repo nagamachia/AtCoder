65871e4764a12175bf9606be70223b95ca31e8a8
nagamachia <7518545@alumni.tus.ac.jp>
2021-07-11 12:16:04 +0900

    solved abc209 d

--- abc209/d.py
+++ abc209/d.py
@@ -18,29 +18,24 @@ cd=[]
 for i in range(Q):
     cd.append(list(map(int,input().split())))
 
-def bfs(C,D):
-    dist = [-1] * (N+1)
-    dist[0] = 0
-    dist[C] = 0
+dist = [-1] * (N+1)
+dist[0] = 0
+dist[1] = 0
 
-    d = deque()
-    d.append(C)
+d = deque()
+d.append(1)
 
-    while d or dist[D]==-1:
-        v = d.popleft()
-        # print("v",v)
-        for i in graph[v]:
-            if dist[i] != -1:
-                continue
-            dist[i] = dist[v] + 1
-            d.append(i)
-    return dist[D]
+while d:
+    v = d.popleft()
+    # print("v",v)
+    for i in graph[v]:
+        if dist[i] != -1:
+            continue
+        dist[i] = dist[v] + 1
+        d.append(i)
 
 for i in range(Q):
-    dist=bfs(cd[i][0],cd[i][1])
-    # print("dist",dist[1:])
-    # print("ans",dist[cd[i][1]])
-    if dist%2==0:
+    if abs(dist[cd[i][0]]-dist[cd[i][1]])%2==0:
         print("Town")
     else:
         print("Road")
