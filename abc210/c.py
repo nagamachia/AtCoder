5827ee20f26ab6dfcffa90abd695a7dffa099260
nagamachia <7518545@alumni.tus.ac.jp>
2021-08-09 18:05:08 +0900

    solved abc210 c

--- abc210/c.py
+++ abc210/c.py
@@ -1,58 +1,24 @@
-# import numpy as np
+from collections import defaultdict
 
 N,K = map(int,input().split())
 
 c = list(map(int,input().split()))
 
+cnt = defaultdict(int)
+
+for i in range(K):
+    cnt[c[i]]+=1
+
 ans=len(set(c[:K]))
-max_idx=[0]
-for i in range(K,N-K+1,K):
-    s=len(set(c[i:i+K]))
-    if s>ans:
-        max_idx=[i]
-        ans=s
-    elif s==ans:
-        max_idx.append(i)
+kinds=ans
 
-s=len(set(c[N-K:]))
-if s>ans:
-    max_idx=[N-K]
-    ans=s
-elif s==ans:
-    max_idx.append(N-K)
-
-r=[]
-for i in max_idx:
-    r+=list(range(max(i-K+1,0),min(i+K-1,N-K+1)))
-r=set(r)
-
-for i in max_idx:
-    r.discard(i)
-
-# for i in max_idx:
-#     for j in range(max(i-K+1,0),min(i+K-1,N-K+1)):
-for j in r:
-    if j==i:
-        continue
-    ans=max(ans,len(set(c[j:j+K])))
+for i in range(N-K):
+    cnt[c[i]]-=1
+    if cnt[c[i]]==0:
+        kinds-=1
+    cnt[c[i+K]]+=1
+    if cnt[c[i+K]]==1:
+        kinds+=1
+    ans = max(ans,kinds)
 
 print(ans)
-# ans=len(set(c[:K]))
-# is_right=True
-# max_idx=0
-
-# for i in range(1,N-K+1):
-#     # print("max_idx:",max_idx)
-#     if is_right:
-#         # print(c[i:i+K])
-#         if c[i] in c[i+1:i+K]:
-#             max_idx=i
-#         else:
-#             max_idx=i
-#             is_right=False
-#     else:
-#         if len(set(c[i:i+K])) >= len(set(c[max_idx:max_idx+K])):
-#             max_idx=i
-#             is_right=True
-
-# print(len(set(c[max_idx:max_idx+K])))
