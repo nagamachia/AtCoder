08a018cc54fe25aa7474cca56e40a4ef56b029dc
nagamachia <7518545@alumni.tus.ac.jp>
2021-08-29 15:57:36 +0900

    solved abc215 d

--- abc215/d.py
+++ abc215/d.py
@@ -1,53 +1,36 @@
 N,M = map(int,input().split())
-
 A = list(map(int,input().split()))
+maxA = max(max(A),M)
 
-def factorization(n):
-    arr = []
-    temp = n
-    for i in range(2, int(-(-n**0.5//1))+1):
-        if temp%i==0:
-            cnt=0
-            while temp%i==0:
-                cnt+=1
-                temp //= i
-            arr.append(i)
-    if temp!=1:
-        arr.append(temp)
-    if arr==[]:
-        arr.append(n)
-    return arr
-
-divs = []
+k = [True]*(maxA+1)
+isPrime = [True]*(maxA+1)
+prime = []
 
 for a in A:
-    divs = divs + factorization(a)
+    k[a] = False
 
-divs = set(divs)
-divs.remove(1)
+for i in range(2,maxA+1):
+    if not isPrime[i]:
+        continue
+    for j in range(i*2,maxA+1,i):
+        isPrime[i] = False
+        k[i] = k[i] and k[j]
+    if not k[i]:
+        prime.append(i)
+
+for p in prime:
+    for j in range(p*2,M+1,p):
+        # k[j] = k[j] * k[p]
+        k[j] = False
 
 ans = [1]
 
-# for m in range(2,M+1):
-#     if divs & set(make_divisors(m)) == set():
-#         ans.append(m)
+for i in range(2,M+1):
+    if k[i]:
+        ans.append(i)
 
-targets = list(range(M+1))
 
-print(divs)
-
-for d in divs:
-    targets = [t for t in targets if t%d!=0]
-
-# while targets!=[]:
-#     m = targets[0]
-#     if divs & set(factorization(m)) == set():
-#         ans.append(m)
-#         targets.remove(m)
-#     else:
-#         targets = [t for t in targets if t%m!=0]
-
-print(len(targets))
-for a in targets:
+print(len(ans))
+for a in ans:
     print(a)
 
